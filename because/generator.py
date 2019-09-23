85e76e3046505827389348b7491b8cf9d91beb66
Guillaume Raffin <guillaume.raffin@grenoble-inp.org>
2019-09-23 13:47:42 +0200

    Fix WalkingGenerator

--- because/generator.py
+++ because/generator.py
@@ -1,5 +1,5 @@
 from dungeon import DungeonLevel
-from geom import Point
+from geom import Vec2D
 import tiles
 
 from random import randint
@@ -26,31 +26,40 @@ class WalkingGenerator:
     Le principe est simple : on commence avec une grille remplie et une position
     aléatoire sur cette grille. On vide cette case puis on se déplace dans une
     direction aléatoire, et on recommence.
+
     :param: width nombre de colonnes de la grille
     :param: height nombre de lignes de la grille
     :param: fill_factor pourcentage de cases à vider, par défaut 0.75
     """
-    def __init__(self, width: int, height: int, empty_factor: float=0.75):
+    def __init__(self, width: int, height: int,
+                 empty_factor=0.75, min_exit_distance=2.0):
         self.width = width
         self.height = height
         self.empty_factor = empty_factor
+        self.min_exit_distance = min_exit_distance
+
+    def _is_valid(self, position: Vec2D):
+        return 0 <= position.x <= self.width and 0 <= position.y < self.height
 
     def generate(self):
         grid_size = self.width * self.height
-        grid = [tiles.EMPTY] * grid_size
+        grid = [tiles.STONE] * grid_size
         entities = []  # TODO: generate entities (later)
 
         target = int(grid_size*self.empty_factor)  # round it down
-        directions = [Point.UP, Point.DOWN, Point.RIGHT, Point.LEFT]
-        pos = Point.random(self.width, self.height)
+        directions = [Vec2D.UP, Vec2D.DOWN, Vec2D.RIGHT, Vec2D.LEFT]
+        pos = Vec2D.random(self.width, self.height)
         i = 0
         while i < target:
-            grid[pos] = tiles.STONE
-            # On va ensuite se déplacer dans une direction aléatoire
-            # Tant que la nouvelle position est invalide, on recommence.
-            next_pos = Point(-1, -1)
-            while not (0 <= next_pos.x < self.width and 0 <= next_pos.y < self.height):
+            # On vide la cellule courante
+            if grid[pos] != tiles.EMPTY:
+                grid[pos] = tiles.EMPTY
+                i += 1
+
+            # On se déplace ensuite dans une direction aléatoire (mais valide).
+            next_pos = Vec2D(-1, -1)
+            while not self._is_valid(next_pos):
                 next_pos = pos + directions[randint(0, 3)]
-            # On met enfin à jour la position de façon valide
             pos = next_pos
+
         return DungeonLevel(grid, entities)
