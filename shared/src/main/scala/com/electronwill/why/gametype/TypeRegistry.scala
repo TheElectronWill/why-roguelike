5cbd0e9bd1c89f457e019d91d3e08c9f474cc6e6
Guillaume Raffin <theelectronwill@gmail.com>
2019-12-17 23:00:20 +0100

    TypeRegistry: ensure that T[X] <: RegisteredType[X] for all X

--- shared/src/main/scala/com/electronwill/why/gametype/TypeRegistry.scala
+++ shared/src/main/scala/com/electronwill/why/gametype/TypeRegistry.scala
@@ -9,19 +9,25 @@ import scala.reflect.ClassTag
  * @tparam I instance type, e.g. `Tile`
  * @tparam T type type, i.e. a type that represent the idea of "type of I".
  *           For instance, if I is `Tile` then T will be `TileType`.
+ * @define iname I
+ * @define itype T
  */
-abstract class TypeRegistry[I, T[+_ <: I] <: RegisteredType[I]] {
-  // --- Type parameters explanation ---
-  // I is the instance type (more precisely, the supertype of all instances we're interested in).
-  // T must be a higher-kinded type that takes one parameter bounded by A, hence [_<:I]
-  // this parameter must be covariant, hence [+_<:I]
-  // And T must be a subtype of RegisteredType[I]
-
+abstract class TypeRegistry[I, T <: [+X <: I] =>> RegisteredType[X]] {
+  /* --- Type parameters explanation ---
+   I is the instance type (more precisely, the supertype of all instances we're interested in).
+   
+   The syntax [X] =>> Y denotes a type lambda that takes one parameter X and gives Y.
+   Here, T must be a higher-kinded type that takes one parameter X, bounded by I,
+   and gives a RegisteredType[X].
+   That is, for all X such that X <: I, T[X] <: RegisteredType[X].
+   Furthermore, X is covariant, meaning that (A <: B) implies (T[A] <: T[B]), which
+   is logical for types (notice how RegisteredType is covariant as well).
+  */
 
   // --- Registry Internals ---
   private val classMap = HashMap[Class[?], T[I]]() // runtime class -> type
   private val idMap = LongMap[T[I]]() // id -> type
-  private var lastId = -1
+  private var lastId = -1 // counter
 
   /** TO IMPLEMENT IN SUBCLASSES: makes an instance of T */
   protected def makeType[Instance <: I](provider: () => Instance, id: Int): T[Instance] // abstract
@@ -43,37 +49,41 @@ abstract class TypeRegistry[I, T[+_ <: I] <: RegisteredType[I]] {
   
 
   // --- Registry Public API ---
-  /** Registers a type of I for which there exist only one instance.
+  /** Registers a type of $iname for which there exist only one instance.
     *
     * @param singleton the instance of this type
-    * @return a new `RegisteredType` that represents the type of this instance 
+    * @return a new $itype that represents the type of this instance 
     */
   def register[Instance <: I](singleton: Instance): T[Instance] =
     val runtimeClass: Class[? <: Instance] = singleton.getClass
     getOrRegister(singleton.getClass, () => singleton)
   
-  /** Registers a type of I for which there can be more than one instance.
+  /** Registers a type of $iname for which there can be more than one instance.
     *
     * @param instanceMaker the function that construct new instances of this type
     * @param tag given (implicit) compile-time information about the class of those instances
-    * @return a new `RegisteredType` that represents this type of I
+    * @return a new $itype that represents this type of I
     */
   def register[Instance <: I](instanceMaker: () => Instance)(given tag: ClassTag[Instance]): T[Instance] =
     val runtimeClass: Class[?] = tag.runtimeClass // Note that runtimeClass doesn't provide any bound
     getOrRegister(runtimeClass, instanceMaker)
   
+  /** Gets an instance of $iname, given we know its precise type at compile time. */
   def create[Instance <: I](given tag: ClassTag[Instance]): Instance =
-    classMap(tag.runtimeClass).asInstanceOf[RegisteredType[Instance]].build()
-    // FIXME ensure that T[Instance] <: RegisteredType[Instance]
+    classMap(tag.runtimeClass).asInstanceOf[T[Instance]].build()
   
+  /** Gets an instance of $iname, given its runtime class. */
   def create(runtimeClass: Class[? <: I]): I =
     classMap(runtimeClass).build()
   
+  /** Gets a $itype, given we know its precise type at compile time. */
   def typeOf[Instance <: I](given tag: ClassTag[Instance]): T[Instance] =
     classMap(tag.runtimeClass).asInstanceOf[T[Instance]]
   
+  /** Gets a $itype, given its runtime class. */
   def typeOf(runtimeClass: Class[? <: I]): T[I] =
     classMap(runtimeClass)
   
+  /** Gets a $itype, given its id. */
   def typeWithId(id: Int): T[I] = idMap(id.toLong)
 }
