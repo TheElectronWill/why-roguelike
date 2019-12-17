b1f7ba03047c8a1ade7c502e8be3045c9b971db4
Guillaume Raffin <theelectronwill@gmail.com>
2019-12-17 22:45:19 +0100

    Clarify TypeRegistry

--- shared/src/main/scala/com/electronwill/why/gametype/RegisteredType.scala
+++ shared/src/main/scala/com/electronwill/why/gametype/RegisteredType.scala
@@ -1,8 +1,8 @@
 package com.electronwill.why.gametype
 
 /** A type of "thing" that has been registered to a registry
- * @tparam B the type of "things" instances
- * @param build function that builds instances of "thing"
- * @param id the type id
- */
+  * @tparam B the type of "things" instances
+  * @param build function that builds instances of "thing"
+  * @param id the type id
+  */
  abstract class RegisteredType[+B](val build: ()=>B, val id: Int)
--- shared/src/main/scala/com/electronwill/why/gametype/TypeRegistry.scala
+++ shared/src/main/scala/com/electronwill/why/gametype/TypeRegistry.scala
@@ -6,72 +6,74 @@ import scala.reflect.ClassTag
 /** A registry for game types. 
  * 
  * @author (c) TheElectronWill and the Tuubes project (https://github.com/tuubes/TuubesCore/pull/59)
- * @tparam A the type of things to register, for instance `Block`
- * @tparam T a class that "materializes" the game type of those things,
-             for instance if A is `Block` then T should be `BlockType`
+ * @tparam I instance type, e.g. `Tile`
+ * @tparam T type type, i.e. a type that represent the idea of "type of I".
+ *           For instance, if I is `Tile` then T will be `TileType`.
  */
-abstract class TypeRegistry[A, T[+_<:A] <: RegisteredType[A]] {
-  private val classMap = HashMap[Class[?], T[A]]() // runtime class -> type
-  private val idMap = LongMap[T[A]]() // id -> type
-  private var lastId = -1
+abstract class TypeRegistry[I, T[+_ <: I] <: RegisteredType[I]] {
+  // --- Type parameters explanation ---
+  // I is the instance type (more precisely, the supertype of all instances we're interested in).
+  // T must be a higher-kinded type that takes one parameter bounded by A, hence [_<:I]
+  // this parameter must be covariant, hence [+_<:I]
+  // And T must be a subtype of RegisteredType[I]
 
-  /** Registers a simple type of "thing" that has no state. */
-  def register[B <: A](singleton: B): T[B] =
-    val provider = ()=>singleton
-    val runtimeClass = singleton.getClass
-    val tag = ClassTag[A](runtimeClass)
-    classMap.getOrElseUpdate(runtimeClass, internalNewType(provider, tag)).asInstanceOf[T[B]]
-
-  /** Registers an advanced type of "thing" that may have an internal state ("stateful").
-   *  It is guaranteed that each thing of that type will be a separate instance.
-   *  Each instance stores its own data independently.
-   */
-  def registerStateful[B <: A](builder: () => B)(given tag: ClassTag[B]): T[B] =
-    classMap.getOrElseUpdate(tag.runtimeClass, internalNewType(builder, tag)).asInstanceOf[T[B]]
-
-  /** Creates a "thing" of some precisely known type.
-   *
-   * @param B the type of thing to create
-   * @param tag tag a `ClassTag` that contains information about the class `B`
-                (automatically provided by the compiler)
-     @return a new thing of type `B`
-   */
-  def create[B <: A](given tag: ClassTag[B]): B =
-    classMap(tag.runtimeClass).asInstanceOf[RegisteredType[B]].build()
-
-  /** Creates a "thing" of some less precise type.
-   *
-   * @param tpe the type of thing to create
-   * @return a new thing
-   */
-  def create[B <: A](tpe: T[B]): B = tpe.build().asInstanceOf[B]
-
-  /** Materializes a type. */
-  def typeOf[B <: A](given tag: ClassTag[B]): T[B] = classMap(tag.runtimeClass).asInstanceOf[T[B]]
-
-  /** Materializes a type. */
-  def typeOf(runtimeClass: Class[_<:A]): T[A] = classMap(runtimeClass).asInstanceOf[T[A]]
-
-  /** Searches a type by internal id. */
-  def typeWithId(id: Int): T[A] = idMap(id.toLong).asInstanceOf[T[A]]
 
   // --- Registry Internals ---
-  /** Creates a new instance of `T` (a "type of thing") for a `B <: A` (the "thing" to be typed).
-   * 
-   * @param provider a function that creates instances of `B` (it provides "things")
-   * @param id the internal id to associate to the new instance of `T`
-   * @return a new registered type
-   */
-  protected def newType[B <: A](provider: ()=>B, id: Int): T[B]
+  private val classMap = HashMap[Class[?], T[I]]() // runtime class -> type
+  private val idMap = LongMap[T[I]]() // id -> type
+  private var lastId = -1
+
+  /** TO IMPLEMENT IN SUBCLASSES: makes an instance of T */
+  protected def makeType[Instance <: I](provider: () => Instance, id: Int): T[Instance] // abstract
 
   /** Generates a new internal id and registers a new type with that id. */
-  protected final def internalNewType[B <: A](provider: ()=>B, tag: ClassTag[B]): T[B] =
+  protected final def makeIdAndType[Instance <: I](provider: () => Instance): T[Instance] =
     // Generates a new type id:
     val id = lastId
     lastId += 1
     // Registers the type with this id:
-    val tpe = newType(provider, id)
+    val tpe = makeType(provider, id)
     idMap(id.toLong) = tpe
     tpe
 
+  protected final def getOrRegister[Instance <: I](runtimeClass: Class[?], instanceMaker: () => Instance) =
+    classMap.getOrElseUpdate(runtimeClass, makeIdAndType(instanceMaker)).asInstanceOf[T[Instance]]
+    // in the map we can only store T[I] not T[Instance] because Instance changes for each entry
+    // that's why an explicit type cast (.asInstanceOf) is needed
+  
+
+  // --- Registry Public API ---
+  /** Registers a type of I for which there exist only one instance.
+    *
+    * @param singleton the instance of this type
+    * @return a new `RegisteredType` that represents the type of this instance 
+    */
+  def register[Instance <: I](singleton: Instance): T[Instance] =
+    val runtimeClass: Class[? <: Instance] = singleton.getClass
+    getOrRegister(singleton.getClass, () => singleton)
+  
+  /** Registers a type of I for which there can be more than one instance.
+    *
+    * @param instanceMaker the function that construct new instances of this type
+    * @param tag given (implicit) compile-time information about the class of those instances
+    * @return a new `RegisteredType` that represents this type of I
+    */
+  def register[Instance <: I](instanceMaker: () => Instance)(given tag: ClassTag[Instance]): T[Instance] =
+    val runtimeClass: Class[?] = tag.runtimeClass // Note that runtimeClass doesn't provide any bound
+    getOrRegister(runtimeClass, instanceMaker)
+  
+  def create[Instance <: I](given tag: ClassTag[Instance]): Instance =
+    classMap(tag.runtimeClass).asInstanceOf[RegisteredType[Instance]].build()
+    // FIXME ensure that T[Instance] <: RegisteredType[Instance]
+  
+  def create(runtimeClass: Class[? <: I]): I =
+    classMap(runtimeClass).build()
+  
+  def typeOf[Instance <: I](given tag: ClassTag[Instance]): T[Instance] =
+    classMap(tag.runtimeClass).asInstanceOf[T[Instance]]
+  
+  def typeOf(runtimeClass: Class[? <: I]): T[I] =
+    classMap(runtimeClass)
+  
+  def typeWithId(id: Int): T[I] = idMap(id.toLong)
 }
